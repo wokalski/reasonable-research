6a5e73cbfb761ed8d1886ea7126de7be548e022f
Wojtek Czekalski <me@wczekalski.com>
2019-11-23 12:23:09 +0100

    Add author's info

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -5,7 +5,7 @@ var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
 var App$ReasonReactExamples = require("./App.bs.js");
 
 function Index$Logo(Props) {
-  return React.createElement("div", undefined, React.createElement("span", {
+  return React.createElement("header", undefined, React.createElement("span", {
                   className: "font-mono text-lg text-gray-800 text-center"
                 }, "Reasonable Research"), React.createElement("a", {
                   className: "bg-white hover:text-red-800 text-red-600 font-normal underline text-md ml-4",
@@ -17,9 +17,42 @@ var Logo = {
   make: Index$Logo
 };
 
-ReactDOMRe.renderToElementWithId(React.createElement("div", {
-          className: "container mt-10 mx-auto sm:mx-4 md:mx-6"
-        }, React.createElement(Index$Logo, { }), React.createElement(App$ReasonReactExamples.make, { })), "index");
+function Index$Footer$Link(Props) {
+  var href = Props.href;
+  var text = Props.text;
+  return React.createElement("a", {
+              className: "text-gray-800 hover:text-black underline text-md",
+              href: href
+            }, text);
+}
+
+var Link = {
+  make: Index$Footer$Link
+};
+
+function Index$Footer(Props) {
+  return React.createElement("footer", {
+              className: "text-gray-800 text-xs font-medium items-end px-2 py-2 bg-red-300"
+            }, React.createElement("span", undefined, "Made by "), React.createElement(Index$Footer$Link, {
+                  href: "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
+                  text: "Marta Konopko"
+                }), React.createElement("span", undefined, " & "), React.createElement(Index$Footer$Link, {
+                  href: "https://twitter.com/wokalski",
+                  text: "Wojtek Czekalski"
+                }));
+}
+
+var Footer = {
+  Link: Link,
+  make: Index$Footer
+};
+
+ReactDOMRe.renderToElementWithId(React.createElement(React.Fragment, {
+          children: null
+        }, React.createElement("div", {
+              className: "flex-grow container mt-10 mx-auto sm:mx-4 md:mx-6"
+            }, React.createElement(Index$Logo, { }), React.createElement("main", undefined, React.createElement(App$ReasonReactExamples.make, { }))), React.createElement(Index$Footer, { })), "index");
 
 exports.Logo = Logo;
+exports.Footer = Footer;
 /*  Not a pure module */
--- src/Index.re
+++ src/Index.re
@@ -1,7 +1,7 @@
 module Logo = {
   [@react.component]
   let make = () =>
-    <div>
+    <header>
       <span className="font-mono text-lg text-gray-800 text-center">
         {React.string("Reasonable Research")}
       </span>
@@ -10,13 +10,38 @@ module Logo = {
         href="/how-to.html">
         {React.string("How to")}
       </a>
-    </div>;
+    </header>;
+};
+
+module Footer = {
+  module Link = {
+    [@react.component]
+    let make = (~href, ~text) =>
+      <a className="text-gray-800 hover:text-black underline text-md" href>
+        {React.string(text)}
+      </a>;
+  };
+  [@react.component]
+  let make = () =>
+    <footer
+      className="text-gray-800 text-xs font-medium items-end px-2 py-2 bg-red-300">
+      <span> {React.string("Made by ")} </span>
+      <Link
+        href="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif"
+        text="Marta Konopko"
+      />
+      <span> {React.string(" & ")} </span>
+      <Link href="https://twitter.com/wokalski" text="Wojtek Czekalski" />
+    </footer>;
 };
 
 ReactDOMRe.renderToElementWithId(
-  <div className="container mt-10 mx-auto sm:mx-4 md:mx-6">
-    <Logo />
-    <App />
-  </div>,
+  <React.Fragment>
+    <div className="flex-grow container mt-10 mx-auto sm:mx-4 md:mx-6">
+      <Logo />
+      <main> <App /> </main>
+    </div>
+    <Footer />
+  </React.Fragment>,
   "index",
 );
